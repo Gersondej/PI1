c81cd186a30b0de1d0b13cf036bf8c069c244213
italopenha <italopenha77@outlook.com>
2025-03-28 21:42:59 -0300

    Alterando HTML do site

--- script.js
+++
@@ -1,6 +0,0 @@
-// Exemplo de interação simples com o formulário
-document.getElementById('contact-form').addEventListener('submit', function (e) {
-    e.preventDefault();
-    alert('Mensagem enviada com sucesso! Entraremos em contato em breve.');
-    this.reset();
-});
